9f60b0e6081cd9ab21c0e350c5d89d06146ec545
Christoph Waffler <waffler.christoph@gmail.com>
2025-04-18 14:28:15 +0200

    fix: clipboard over ssh

--- init.lua
+++ init.lua
@@ -128,7 +128,29 @@ end
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+
+-- Check if in SSH session
+if vim.env.SSH_TTY then
+  -- Use OSC 52 clipboard when in SSH
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = function()
+        return { vim.fn.split(vim.fn.getreg '', '\n'), vim.fn.getregtype '' }
+      end,
+      ['*'] = function()
+        return { vim.fn.split(vim.fn.getreg '', '\n'), vim.fn.getregtype '' }
+      end,
+    },
+  }
+else
+  -- Use regular system clipboard when local
+  vim.opt.clipboard = 'unnamedplus'
+end
 
 -- Prevent deleted text from being copied to the clipboard
 vim.keymap.set({ 'n', 'x' }, 'd', '"_d')
