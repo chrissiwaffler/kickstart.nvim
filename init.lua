a5c96c286058f8fb5bc7c662d2393ae11df4a4f6
Christoph Waffler <waffler.christoph@gmail.com>
2026-07-09 04:39:45 +0200

    fix: migrate treesitter config to main branch rewrite API
    The nvim-treesitter main branch is a full rewrite that removes nvim-treesitter.configs. Migrate to the new API: require('nvim-treesitter').setup, vim.treesitter.start() via FileType autocmd, explicit textobjects keymaps, and remove TSDisable (tex excluded from FileType pattern instead).
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- init.lua
+++ init.lua
@@ -1284,66 +1284,57 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'python', 'javascript', 'typescript' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-      fold = {
-        enable = true,
-        disable = {},
-      },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    config = function()
+      require('nvim-treesitter').setup {}
 
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'python', 'javascript', 'typescript' }
+      require('nvim-treesitter').install(parsers):wait(300000)
 
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'bash', 'c', 'cpp', 'css', 'diff', 'html', 'javascript', 'json', 'lua', 'luadoc', 'markdown', 'python', 'typescript', 'vim', 'vimdoc', 'yaml' },
+        callback = function() vim.treesitter.start() end,
+      })
+
+      vim.o.foldmethod = 'expr'
+      vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     end,
   },
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
-      require('nvim-treesitter.configs').setup {
-        textobjects = {
-          move = {
-            enable = true,
-            goto_next_start = {
-              [']f'] = '@function.outer',
-              [']c'] = '@class.outer',
-            },
-            goto_previous_start = {
-              ['[f'] = '@function.outer',
-              ['[c'] = '@class.outer',
-            },
-            goto_next_end = {
-              [']F'] = '@function.outer',
-              [']C'] = '@class.outer',
-            },
-            goto_previous_end = {
-              ['[F'] = '@function.outer',
-              ['[C'] = '@class.outer',
-            },
-          },
+      require('nvim-treesitter-textobjects').setup {
+        move = {
+          set_jumps = true,
         },
       }
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
+        require('nvim-treesitter-textobjects.move').goto_next_start('@function.outer', 'textobjects')
+      end, { desc = 'Next function start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ']c', function()
+        require('nvim-treesitter-textobjects.move').goto_next_start('@class.outer', 'textobjects')
+      end, { desc = 'Next class start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
+        require('nvim-treesitter-textobjects.move').goto_previous_start('@function.outer', 'textobjects')
+      end, { desc = 'Previous function start' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[c', function()
+        require('nvim-treesitter-textobjects.move').goto_previous_start('@class.outer', 'textobjects')
+      end, { desc = 'Previous class start' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']F', function()
+        require('nvim-treesitter-textobjects.move').goto_next_end('@function.outer', 'textobjects')
+      end, { desc = 'Next function end' })
+      vim.keymap.set({ 'n', 'x', 'o' }, ']C', function()
+        require('nvim-treesitter-textobjects.move').goto_next_end('@class.outer', 'textobjects')
+      end, { desc = 'Next class end' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[F', function()
+        require('nvim-treesitter-textobjects.move').goto_previous_end('@function.outer', 'textobjects')
+      end, { desc = 'Previous function end' })
+      vim.keymap.set({ 'n', 'x', 'o' }, '[C', function()
+        require('nvim-treesitter-textobjects.move').goto_previous_end('@class.outer', 'textobjects')
+      end, { desc = 'Previous class end' })
     end,
   },
   {
@@ -1410,14 +1401,6 @@ require('lazy').setup({
         sections = 1,
         styles = 1,
       }
-
-      -- Disable treesitter for tex files
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'tex', 'latex' },
-        callback = function()
-          vim.cmd 'TSDisable highlight'
-        end,
-      })
 
       -- Mappings
       vim.api.nvim_create_autocmd('FileType', {
